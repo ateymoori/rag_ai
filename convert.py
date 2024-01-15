fb48391aa69f22a476fa5eddf21b3d35589bf09e
AmirHossein Teymoori <teymoori.net@gmail.com>
2024-01-15 19:32:41 +0000

    changes

--- convert.py
+++ convert.py
@@ -1,27 +1,55 @@
+import logging
+from tqdm import tqdm
 import os
 import time
 import chromadb
 import textwrap
+import subprocess
+from langchain_community.llms import LlamaCpp
 from langchain.embeddings import LlamaCppEmbeddings
-from langchain.embeddings import LlamaCppEmbeddings
-import logging
-from tqdm import tqdm
 
-MODEL_REPO = "TheBloke/Llama-2-7b-Chat-GGUF"
-MODEL_NAME = "llama-2-7b-chat.Q4_K_M.gguf"
+MODEL_REPO = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
+MODEL_NAME = "mistral-7b-instruct-v0.1.Q4_K_M.gguf"
 LOCAL_DIR = "/models"
 CHORMADB_PATH = 'chroma_db_data'
 CHORMADB_COLLECTION_NAME = 'web_pages'
-N_CTX = 4096  
-N_THREADS = 10   
-N_GPU_LAYERS = 0  
-MAX_TOKENS = 256  
-TEMPERATURE = 0.1 
 
-# Initialize logging
+model_config = {
+    "n_ctx": 4096,
+    "n_threads": 10,
+    "n_gpu_layers": 0,
+    "max_tokens": 256,
+    "temperature": 0.1
+}
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def download_model():
+    model_path = os.path.join(LOCAL_DIR, MODEL_NAME)
+    if not os.path.exists(model_path):
+        logger.info(f"Downloading model {MODEL_NAME} from {MODEL_REPO}...")
+        command = [
+            "huggingface-cli", "download", MODEL_REPO,
+            MODEL_NAME, "--local-dir", LOCAL_DIR,
+            "--local-dir-use-symlinks", "False"
+        ]
+        subprocess.run(command, check=True)
+    else:
+        logger.info("Model file already exists. Skipping download.")
+
+def initialize_model():
+    llm = LlamaCpp(
+        model_path=os.path.join(LOCAL_DIR, MODEL_NAME),
+        n_ctx=model_config["n_ctx"],
+        n_threads=model_config["n_threads"],
+        n_gpu_layers=model_config["n_gpu_layers"],
+        stream=True,
+        f16_kv=True,
+        top_p=1,
+        temperature=model_config["temperature"],
+        verbose=True
+    )
+    return llm
 
 def convert_pages_to_chromadb():
     chroma_client = chromadb.PersistentClient(path=CHORMADB_PATH)
@@ -65,5 +93,8 @@ def generate_embeddings(chunks, pbar):
 
     return embeddings
     
+    
+download_model()
+initialize_model()
     
 convert_pages_to_chromadb()
